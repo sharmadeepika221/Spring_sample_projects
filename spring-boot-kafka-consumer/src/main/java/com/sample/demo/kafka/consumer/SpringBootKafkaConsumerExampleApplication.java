25e23169d93b5a4c01883a6dffefd6ae8c45886c
Deepika Sharma <Swasti@01>
2019-02-14 21:38:48 +0100

    Comitting Spring boot project with Kafka Consumer

---
+++ spring-boot-kafka-consumer/src/main/java/com/sample/demo/kafka/consumer/SpringBootKafkaConsumerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.sample.demo.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaConsumerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaConsumerExampleApplication.class, args);
+	}
+}
---
+++ spring-boot-kafka-consumer/src/main/java/com/sample/demo/kafka/consumer/listener/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.sample.demo.kafka.consumer.listener;
+
+import com.sample.demo.kafka.consumer.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka_Example", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "Kafka_Example_json", groupId = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+}
