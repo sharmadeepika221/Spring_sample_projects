1ebb247a3f6dbec648789777cd6e8c6ac1aa541b
Deepika Sharma <Swasti@01>
2019-02-14 22:48:15 +0100

    Changes done for spring project with kafka producer

---
+++ spring-boot-kafka-producer/src/main/java/com/sample/demo/kafka/kafkaproducer/SpringBootKafkaProducerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.sample.demo.kafka.kafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaProducerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaProducerExampleApplication.class, args);
+	}
+}
